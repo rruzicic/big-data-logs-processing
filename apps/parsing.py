883670e799439038862ba596c1d2eff1ddc5c643
Ratko Ruzicic <ratko.ruzicic@oaklandgrp.com>
2024-03-29 21:59:02 +0100

    Add main parsing script, use grafana

---
+++ apps/parsing.py
@@ -0,0 +1,141 @@
+#!/usr/bin/python
+
+import os
+import re
+from datetime import datetime
+from pyspark.sql import SparkSession, Row
+from pyspark.sql.functions import to_date, col, window
+from pyspark.sql.types import StructType, StructField, StringType, TimestampType, IntegerType
+
+
+def parse_user_agent(user_agent_string):
+    browser_pattern = re.compile(r"(\w+)/(.*?)\s")
+    os_pattern = re.compile(r"\((.*?)\)")
+    
+    browser_match = browser_pattern.search(user_agent_string)
+    os_match = os_pattern.search(user_agent_string)
+
+    browser_family = browser_match.group(1) if browser_match else None
+    browser_version = browser_match.group(2) if browser_match else None
+    os_info = os_match.group(1) if os_match else None
+
+    if os_info != None:
+        os_info = os_info.split(";")[0]
+
+    return {
+        "browser_family": browser_family,
+        "browser_version": browser_version,
+        "os_info": os_info
+    }
+
+def parse_log_row(s) -> Row:
+    parsedRow = nginx_log_row(s)
+    splitRequest = parsedRow[4].split(' ')
+    parsedUserAgent = parse_user_agent(parsedRow[8])
+    emptyRow = Row(
+        "",
+        "",
+        "",
+        datetime.strptime("22/Jan/2019:03:56:34 +0330", "%d/%b/%Y:%H:%M:%S %z"),
+        "",
+        "",
+        "",
+        "",
+        0,
+        "",
+        "",
+        "",
+        "",
+        ""
+    )
+    if len(parsedRow) != 10:
+        return emptyRow
+    if len(splitRequest) != 3:
+        return emptyRow
+
+    return Row(
+        parsedRow[0],
+        parsedRow[1],
+        parsedRow[2],
+        datetime.strptime(parsedRow[3], "%d/%b/%Y:%H:%M:%S %z"),
+        splitRequest[0],
+        splitRequest[1],
+        splitRequest[2],
+        parsedRow[5],
+        int(parsedRow[6]),
+        parsedRow[7],
+        parsedUserAgent["browser_family"],
+        parsedUserAgent["browser_version"],
+        parsedUserAgent["os_info"],
+        parsedRow[9]
+    )
+
+def nginx_log_row(s):
+    row = [ ]
+    qe = qp = None # quote end character (qe) and quote parts (qp)
+    for s in s.replace('\r','').replace('\n','').split(' '):
+        if qp:
+            qp.append(s)
+        elif '' == s: # blanks
+            row.append('')
+        elif '"' == s[0]: # begin " quote "
+            qp = [ s ]
+            qe = '"'
+        elif '[' == s[0]: # begin [ quote ]
+            qp = [ s ]
+            qe = ']'
+        else:
+            row.append(s)
+
+        l = len(s)
+        if l and qe == s[-1]: # end quote
+            if l == 1 or s[-2] != '\\': # don't end on escaped quotes
+                row.append(' '.join(qp)[1:-1].replace('\\'+qe, qe))
+                qp = qe = None
+    return row
+
+def quiet_logs(sc):
+  logger = sc._jvm.org.apache.log4j
+  logger.LogManager.getLogger("org"). setLevel(logger.Level.ERROR)
+  logger.LogManager.getLogger("akka").setLevel(logger.Level.ERROR)
+
+spark = SparkSession \
+    .builder \
+    .appName("Python Spark SQL - Create DataFrame") \
+    .getOrCreate()
+
+quiet_logs(spark)
+
+HDFS_NAMENODE = os.environ["CORE_CONF_fs_defaultFS"]
+
+logsSchema = StructType([
+    StructField("remote_addr", StringType(), True),
+    StructField("server_name", StringType(), True),
+    StructField("remote_user", StringType(), True),
+    StructField("time_local", TimestampType(), True),
+    StructField("method", StringType(), True),
+    StructField("path", StringType(), True),
+    StructField("type", StringType(), True),
+    StructField("status", StringType(), True),
+    StructField("body_bytes_sent", IntegerType(), True),
+    StructField("http_referer", StringType(), True),
+    StructField("browser_family", StringType(), True),
+    StructField("browser_version", StringType(), True),
+    StructField("os", StringType(), True),
+    StructField("http_x_forwarded_for", StringType(), True),
+])
+
+dfFromTXT = spark.createDataFrame(
+  spark.sparkContext.textFile(HDFS_NAMENODE + "/input/access.log")
+  .map(lambda x: parse_log_row(x)),
+  logsSchema
+)
+
+# dfFromTXT.show()
+# dfDistinct = dfFromTXT.groupBy('http_user_agent.os').count()
+
+dfFromTXT \
+    .write \
+    .mode("overwrite") \
+    .option("driver", "org.postgresql.Driver") \
+    .jdbc("jdbc:postgresql://postgres/postgres", "public.parsed", properties={"user": "postgres", "password": "pass"}) \
---
+++ sql/01-common-ips.sql
@@ -0,0 +1,87 @@
+-- 1. 
+select p.remote_addr, count(p.remote_addr)
+from parsed p 
+group by p.remote_addr;
+
+-- 2.
+
+select 
+	extract (hour from date_trunc('hour', p.time_local)) as req_hour,
+	count(*)
+from parsed p
+group by req_hour
+order by req_hour;
+
+-- 3.
+
+select p.method, count(p.method)
+from parsed p 
+group by p.method;
+
+-- 4.
+
+select 
+	count(*) as cnt,
+	100 * (count(*) * 1.0 / sum(count(*)) over (partition by a.nr)),
+	a.elem,
+	a.nr as level
+	-- split_part(p.path, '?', 1) as parsed_path
+from parsed p
+left join lateral string_to_table(split_part(p.path, '?', 1), '/') with ordinality as a(elem, nr) on true
+group by level, a.elem
+order by level asc, cnt desc;
+
+-- 5.
+
+select p.os, count(p.os)
+from parsed p 
+group by p.os;
+
+-- 6.
+
+select p.browser_family, count(p.browser_family)
+from parsed p 
+group by p.browser_family;
+
+-- 7.
+
+select p.status, count(p.status)
+from parsed p 
+group by p.status
+order by p.status;
+
+-- 8.
+-- ovo moze da se nadogradi tako sto se uzima samo host address od referera bez putanje
+select p.http_referer, count(p.http_referer)
+from parsed p 
+group by p.http_referer;
+
+-- 9.
+
+select 
+    sum(p.body_bytes_sent) / 1024 as data_kb, 
+    p.path
+from parsed p 
+group by p.path
+order by sum(p.body_bytes_sent) desc;
+
+-- 10.
+
+select sum(p.body_bytes_sent), p.remote_addr
+from parsed p 
+group by p.remote_addr
+order by sum(p.body_bytes_sent) desc;
+
+-- 11. 
+
+-- windowed upit koji za cilj ima da nadje top 3 zahteva sa najduzim odgovorom za svaki http referer
+
+SELECT referer, bytes_sent, pos
+FROM
+  (SELECT split_part(split_part(p.http_referer,'//',2), '/', 1) as referer, p.body_bytes_sent as bytes_sent,
+		rank() OVER (PARTITION BY split_part(split_part(p.http_referer,'//',2), '/', 1) ORDER BY p.body_bytes_sent DESC) AS pos
+     FROM parsed p
+  ) AS ss
+WHERE pos <= 3;
+
+-- 12. 
